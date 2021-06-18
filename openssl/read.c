c4684407d31b0158fb69c96ecb2d80395d2918ba
Jesse Hathaway <jesse@mbuki-mvuki.org>
2021-06-18 10:09:55 -0500

    add subject & issuer output!!

--- openssl/read.c
+++ openssl/read.c
@@ -7,6 +7,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct stack_st {
+  int num;
+  const void **data;
+  int sorted;
+  int num_alloc;
+  OPENSSL_sk_compfunc comp;
+};
+
+void *sk_value(const struct stack_st *stptr, int i) {
+  struct stack_st st;
+  memcpy(&st, (void *)stptr, sizeof(struct stack_st));
+  if (i < 0 || i >= st.num)
+    return NULL;
+  return (void *)st.data[i];
+}
+
+int x509_name(char *output, int output_len, X509_NAME *nameptr) {
+  X509_NAME name;
+  memcpy(&name, (void *)nameptr, sizeof(name));
+  STACK_OF(X509_NAME_ENTRY) * name_stackptr;
+  name_stackptr = name.entries;
+  int num = sk_X509_NAME_ENTRY_num(name_stackptr);
+  int i = 0;
+  struct stack_st st;
+  memcpy(&st, (void *)name_stackptr, sizeof(struct stack_st));
+  X509_NAME_ENTRY *name_tmp_ptr = NULL, name_tmp;
+  ASN1_STRING *asn1_str_tmp_ptr = NULL, asn1_str_tmp;
+  int pos = 0;
+  for (i = 0; i < st.num; i++) {
+    name_tmp_ptr = sk_value((const struct stack_st *)name_stackptr, i);
+    memcpy(&name_tmp, (void *)name_tmp_ptr, sizeof(struct X509_name_entry_st));
+    memcpy(&asn1_str_tmp, (void *)name_tmp.value, sizeof(ASN1_STRING));
+    memcpy(&asn1_str_tmp, (void *)name_tmp.value, sizeof(ASN1_STRING));
+    memcpy(output + pos, (void *)asn1_str_tmp.data, asn1_str_tmp.length);
+    pos += asn1_str_tmp.length;
+    if ((i + 1) < st.num) {
+      output[pos] = ',';
+      pos++;
+      output[pos] = ' ';
+      pos++;
+    }
+    output[pos] = '\0';
+  }
+  return 0;
+}
+
 int main() {
   char *path;
   path = "google.pem";
@@ -15,14 +61,10 @@ int main() {
     fprintf(stderr, "unable to open: %s\n", path);
     return EXIT_FAILURE;
   }
-  X509 *certptr;
-  X509 cert;
+  X509 *certptr, cert;
   X509_VAL validity;
-  ASN1_TIME *asn1_timeptr;
-  ASN1_TIME asn1_time;
-  X509_NAME *nameptr;
-  X509_NAME name;
-  STACK_OF(X509_NAME_ENTRY) * name_stackptr;
+  ASN1_TIME *asn1_timeptr, asn1_time;
+  X509_NAME *nameptr, name;
   certptr = PEM_read_X509(fp, NULL, NULL, NULL);
   if (!certptr) {
     fprintf(stderr, "unable to parse certificate in: %s\n", path);
@@ -31,20 +73,27 @@ int main() {
   }
   memcpy(&cert, (void *)certptr, sizeof(cert));
   validity = cert.cert_info.validity;
+
+  asn1_timeptr = validity.notBefore;
+  memcpy(&asn1_time, (void *)asn1_timeptr, sizeof(asn1_time));
+  printf("Not Before: '%s'\n", asn1_time.data);
+
   asn1_timeptr = validity.notAfter;
   memcpy(&asn1_time, (void *)asn1_timeptr, sizeof(asn1_time));
-  printf("ASN1 %s\n", asn1_time.data);
+  printf("Not After: '%s'\n", asn1_time.data);
+
   nameptr = cert.cert_info.subject;
-  memcpy(&name, (void *)nameptr, sizeof(name));
-  name_stackptr = name.entries;
-  int num = sk_X509_NAME_ENTRY_num(name_stackptr);
-  printf("NUM %d\n", num);
-  int i = 0;
-  X509_NAME_ENTRY *name_tmp = NULL;
-  for (i = 0; i < sk_X509_NAME_ENTRY_num(name_stackptr); i++) {
-    name_tmp = sk_X509_NAME_ENTRY_value(name_stackptr, i);
-    printf("XXX = '%s'\n", name_tmp->value->data);
-  }
+  int subject_len = 200;
+  char subject[subject_len];
+  x509_name(subject, subject_len, nameptr);
+  printf("subject: '%s'\n", subject);
+
+  nameptr = cert.cert_info.issuer;
+  int issuer_len = 200;
+  char issuer[issuer_len];
+  x509_name(issuer, issuer_len, nameptr);
+  printf("issuer: '%s'\n", issuer);
+
   X509_free(certptr);
   fclose(fp);
 }
