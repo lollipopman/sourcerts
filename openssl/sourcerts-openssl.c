628f931960ef0e9950ea893077b15b3f018aa6f9
Jesse Hathaway <jesse@mbuki-mvuki.org>
2021-06-18 10:09:55 -0500

    resync with bpf version

--- openssl/sourcerts-openssl.c
+++ openssl/sourcerts-openssl.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,12 +7,15 @@
 #include "sc_openssl/x509.h"
 /* #include <openssl/pem.h> */
 
+#define X509_NAME_SIZE 1024
+
 struct sourcerts_event_t {
+  uint32_t pid;
   /* asn1_time, e.g. 201020180834Z */
   char not_before[14];
   char not_after[14];
-  char subject[200];
-  char issuer[200];
+  char subject[X509_NAME_SIZE];
+  char issuer[X509_NAME_SIZE];
 } __attribute__((packed));
 
 static __always_inline void perf_submit(struct sourcerts_event_t *, size_t);
@@ -32,36 +36,65 @@ static __always_inline int sc_x509_name(char *output, int output_len,
   memcpy(&name, (void *)nameptr, sizeof(name));
   OPENSSL_STACK *name_stackptr;
   name_stackptr = (OPENSSL_STACK *)name.entries;
-  int num = OPENSSL_sk_num(name_stackptr);
-  int i = 0;
+  unsigned int i = 0;
   struct stack_st st;
   memcpy(&st, (void *)name_stackptr, sizeof(struct stack_st));
   struct X509_name_entry_st *name_tmp_ptr = NULL, name_tmp;
   ASN1_STRING *asn1_str_tmp_ptr = NULL, asn1_str_tmp;
-  int pos = 0;
-  unsigned int rlen = 0;
-  for (i = 0; i < st.num; i++) {
-    name_tmp_ptr = sc_sk_value((const struct stack_st *)name_stackptr, i);
+  unsigned int pos = 0;
+  unsigned int asn1_str_len;
+  unsigned int st_len;
+  for (i = 0; i < (X509_NAME_SIZE / 66); i++) {
+    /* XXX combine above? */
+    if (i > (st.num - 1)) {
+      break;
+    }
+    printf("I '%d'\n", i);
+    name_tmp_ptr = sc_sk_value(name_stackptr, i);
     if (!name_tmp_ptr)
       return 0;
     memcpy(&name_tmp, (void *)name_tmp_ptr, sizeof(struct X509_name_entry_st));
     memcpy(&asn1_str_tmp, (void *)name_tmp.value, sizeof(ASN1_STRING));
-    memcpy(output + pos, (void *)asn1_str_tmp.data, asn1_str_tmp.length);
-    rlen = (unsigned int)asn1_str_tmp.length;
-    pos += rlen;
+    if ((unsigned int)asn1_str_tmp.length < 64) {
+      asn1_str_len = (unsigned int)asn1_str_tmp.length;
+    } else {
+      printf("ASN string length too long '%d'\n",
+             (unsigned int)asn1_str_tmp.length);
+      return 0;
+    }
+    printf("AS1 LEN '%d'\n", asn1_str_len);
+    printf("POS '%d'\n", pos);
+    unsigned int rlen;
+    if (pos < (output_len - 64)) {
+      /* XXX is this math right?? */
+      rlen = X509_NAME_SIZE - 1 - 64 * i;
+      memcpy(output + pos, (void *)asn1_str_tmp.data,
+             asn1_str_len & (X509_NAME_SIZE - 1));
+      pos += asn1_str_len;
+    }
+    /*
+     * XXX DOES NOT WORK blarg!!
+     * need to understand the bounds checking better!!
+     */
     if ((i + 1) < st.num) {
-      output[pos] = ',';
+      if (pos < output_len) {
+        output[pos] = ',';
+      }
       pos++;
-      output[pos] = ' ';
+      if (pos < output_len) {
+        output[pos] = ' ';
+      }
       pos++;
     }
   }
-  output[pos] = '\0';
-  return 0;
+  if (pos < output_len) {
+    output[pos] = '\0';
+  }
+  printf("OUTPUT '%s'\n", output);
+  return 1;
 }
 
-int get_return_value(X509 *certptr) {
-  struct sourcerts_event_t event = {};
+int get_return_value(X509 *certptr, struct sourcerts_event_t *event) {
   X509 cert;
   memcpy(&cert, (void *)certptr, sizeof(cert));
   X509_VAL validity;
@@ -70,18 +103,20 @@ int get_return_value(X509 *certptr) {
   validity = cert.cert_info.validity;
 
   memcpy(&asn1_time, validity.notBefore, sizeof(asn1_time));
-  memcpy(&event.not_before, asn1_time.data, sizeof(event.not_before));
-  event.not_before[sizeof(event.not_before) - 1] = '\0';
+  memcpy(&event->not_before, asn1_time.data, sizeof(event->not_before));
+  event->not_before[sizeof(event->not_before) - 1] = '\0';
 
   memcpy(&asn1_time, validity.notAfter, sizeof(asn1_time));
-  memcpy(&event.not_after, asn1_time.data, sizeof(event.not_before));
-  event.not_before[sizeof(event.not_after) - 1] = '\0';
+  memcpy(&event->not_after, asn1_time.data, sizeof(event->not_before));
+  event->not_before[sizeof(event->not_after) - 1] = '\0';
 
-  sc_x509_name(event.subject, sizeof(event.subject), cert.cert_info.subject);
+  if (!sc_x509_name(event->subject, sizeof(event->subject),
+                    cert.cert_info.subject)) {
+    printf("ERROR\n");
+  }
+  sc_x509_name(event->issuer, sizeof(event->issuer), cert.cert_info.issuer);
 
-  sc_x509_name(event.issuer, sizeof(event.issuer), cert.cert_info.issuer);
-
-  perf_submit(&event, sizeof(event));
+  perf_submit(event, sizeof(&event));
   return 0;
 }
 
@@ -110,7 +145,8 @@ int main() {
     fclose(fp);
     return EXIT_FAILURE;
   }
-  get_return_value(certptr);
+  struct sourcerts_event_t event = {};
+  get_return_value(certptr, &event);
   X509_free(certptr);
   fclose(fp);
 }
