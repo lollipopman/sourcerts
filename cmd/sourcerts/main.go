88e349220ef6b52cbf6dfb4d9ee3f4cec3cd890b
Jesse Hathaway <jesse@mbuki-mvuki.org>
2021-06-18 10:09:55 -0500

    use tabs in C code

--- cmd/sourcerts/main.go
+++ cmd/sourcerts/main.go
@@ -17,31 +17,31 @@ const source string = `
 #include <partial_x509.h>
 
 struct event_t {
-        u32 pid;
-        char str[80];
+	u32 pid;
+	char str[80];
 } __attribute__((packed));
 
 BPF_PERF_OUTPUT(sourcerts_events);
 
 int get_return_value(struct pt_regs *ctx) {
-        struct event_t event = {};
-        u32 pid;
-				X509 *certptr;
-				X509 cert;
-				X509_VAL validity;
-				ASN1_TIME *asn1_timeptr;
-				ASN1_TIME asn1_time;
-        if (!PT_REGS_RC(ctx))
-                return 0;
-        pid = bpf_get_current_pid_tgid();
-        event.pid = pid;
-        bpf_probe_read(&cert, sizeof(cert), (void *)PT_REGS_RC(ctx));
-				validity = cert.cert_info.validity;
-				asn1_timeptr = validity.notAfter;
-        bpf_probe_read(&asn1_time, sizeof(asn1_time), (void *)asn1_timeptr);
-        bpf_probe_read_str(&event.str, 80, (void *)asn1_time.data);
-        sourcerts_events.perf_submit(ctx, &event, sizeof(event));
-        return 0;
+	struct event_t event = {};
+	u32 pid;
+	X509 *certptr;
+	X509 cert;
+	X509_VAL validity;
+	ASN1_TIME *asn1_timeptr;
+	ASN1_TIME asn1_time;
+	if (!PT_REGS_RC(ctx))
+		return 0;
+	pid = bpf_get_current_pid_tgid();
+	event.pid = pid;
+	bpf_probe_read(&cert, sizeof(cert), (void *)PT_REGS_RC(ctx));
+	validity = cert.cert_info.validity;
+	asn1_timeptr = validity.notAfter;
+	bpf_probe_read(&asn1_time, sizeof(asn1_time), (void *)asn1_timeptr);
+	bpf_probe_read_str(&event.str, 80, (void *)asn1_time.data);
+	sourcerts_events.perf_submit(ctx, &event, sizeof(event));
+	return 0;
 }
 `
 
